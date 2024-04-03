e98a51a96b63cdce4654298b4ac422387575eb72
Kawan-de-Sa <kawan.ferreira@mail.uft.edu.br>
2024-04-02 23:47:14 -0300

    Correção

--- ContaBanco/src/contaTerminal/java/ContaTerminal.java
+++ ContaBanco/src/contaTerminal/java/ContaTerminal.java
@@ -6,15 +6,12 @@ package contaTerminal.java;
 		public class ContaTerminal {
 		    public static void main(String[] args) {
 		        Scanner scanner = new Scanner(System.in);
-
-		        System.out.println("Bem-vindo ao sistema de registro do banco.");
-
-		        // Registro do cliente
-		        System.out.println("Por favor, insira os seguintes dados do cliente:");
-
+		        
+		        System.out.println("Por favor, insira os respectivos dados:");
+		        
 		        System.out.print("Número de identificação do usuário: ");
 		        int numeroIdentificacao = scanner.nextInt();
-		        scanner.nextLine(); // Limpar o buffer do scanner
+		        scanner.nextLine(); 
 
 		        System.out.print("Agência do usuário: ");
 		        String agencia = scanner.nextLine();
@@ -25,12 +22,7 @@ package contaTerminal.java;
 		        System.out.print("Saldo da conta: ");
 		        double saldoConta = scanner.nextDouble();
 
-		        // Mostrar informações do cliente
-		        System.out.println("\nInformações do cliente registradas com sucesso:");
-		        System.out.println("Número de identificação do usuário: " + numeroIdentificacao);
-		        System.out.println("Agência do usuário: " + agencia);
-		        System.out.println("Nome do cliente: " + nomeCliente);
-		        System.out.println("Saldo da conta: " + saldoConta);
+		        System.out.println("Olá" + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia é:" + agencia + "conta:" + numeroIdentificacao + "e seu saldo" + saldoConta + ", ja esta disponivel para saque.");
 
 		        scanner.close();
 		    }
